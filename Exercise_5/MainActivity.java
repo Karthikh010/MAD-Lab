2546892d534148bd5eb1fe9f3005847f077e6c51
Karthik H <74698680+Karthikh010@users.noreply.github.com>
2021-12-31 20:17:22 +0530

    Update MainActivity.java

--- Exercise_5/MainActivity.java
+++ Exercise_5/MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.myapp;
+package com.example.exercise5;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button viewToast = findViewById(R.id.viewToast);
+        viewToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hey, you clicked the button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
