3f6635ff813adecc006b6f592321027a8e143189
Karthik H <74698680+Karthikh010@users.noreply.github.com>
2022-01-06 14:46:38 +0530

    Add files via upload

---
+++ Exercise_7/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.table;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button click=findViewById(R.id.b1);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "you have submitted the details", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
